c79f42b9cfae022af52659bff099d264226e4464
uditi sharma <162996543+uditisharmaaa@users.noreply.github.com>
2024-12-10 00:03:31 +0400

    Fix paths

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Portfolio from "./Portfolio"; // Portfolio component
 import BlogSection from "./BlogSection"; // Blog list page
 import BlogPostPage from "./BlogPostPage"; // Individual blog post page
 
 const App = () => {
   return (
-    <Router basename="/portfolio"> {/* Add the basename for GitHub Pages */}
+    <HashRouter>
       <Routes>
         {/* Ensure all paths match the basename */}
         <Route path="/" element={<Portfolio />} />
         <Route path="/blog" element={<BlogSection />} />
         <Route path="/blog/:id" element={<BlogPostPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
