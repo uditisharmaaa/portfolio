70b74a7270ff9fbde27fa5bbd0e1f2bb62cfecb1
uditi sharma <162996543+uditisharmaaa@users.noreply.github.com>
2024-12-10 00:18:07 +0400

    Fix path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import Portfolio from "./Portfolio"; // Portfolio component
 import BlogSection from "./BlogSection"; // Blog list page
